e39facafb95004258c77260163e5389ab1b48bf2
Richie911 <oohrichmond.k@gmail.com>
2025-06-07 13:46:06 +0000

    add workflow canvas component

---
+++ components/workflow/workflowCanvas.tsx
@@ -0,0 +1,86 @@
+'use client';
+
+import React, { useCallback, useState } from 'react';
+import {
+  ReactFlow,
+  MiniMap,
+  Controls,
+  Background,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+  OnConnect,
+  BackgroundVariant,
+  ReactFlowInstance,
+  Panel,
+  Node,
+} from '@xyflow/react';
+
+import '@xyflow/react/dist/style.css';
+import { Button } from '@/components/ui/button';
+
+const initialNodes: Node[] = [
+  {
+    id: '1',
+    position: { x: 0, y: 0 },
+    data: { label: '1' },
+  },
+  { id: '2', position: { x: 100, y: 100 }, data: { label: '2' } },
+
+  { id: '3', position: { x: 20, y: 550 }, data: { label: '3' } },
+
+  { id: '4', position: { x: 660, y: 100 }, data: { label: '4' } },
+
+  { id: '5', position: { x: 459, y: 300 }, data: { label: '5' } },
+];
+const initialEdges = [{ id: 'e1-2', source: '1', target: '5' }];
+
+export default function Workflow() {
+  const [nodes, , onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const [rfInstance, setRfInstance] = useState<ReactFlowInstance>();
+
+  const onConnect: OnConnect = useCallback(
+    (params) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges]
+  );
+
+  const onSave = useCallback(() => {
+    if (rfInstance) {
+      const flow = rfInstance.toObject();
+      console.log('Flow:', flow);
+      //   localStorage.setItem('flowKey', JSON.stringify(flow));
+    }
+  }, [rfInstance]);
+
+  // const nodeTypes = { textUpdater: NodeWrapper };
+
+  return (
+    <div style={{ width: '100vw', height: '100dvh' }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        // nodeTypes={nodeTypes}
+        fitView
+        onInit={(e) => setRfInstance(e as unknown as ReactFlowInstance)}
+      >
+        <Controls />
+        <MiniMap />
+        <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
+        <Panel position="top-right">
+          <div className="flex gap-4">
+            <Button onClick={onSave} className="cursor-pointer">
+              Create
+            </Button>
+            <Button onClick={onSave} className="cursor-pointer">
+              save
+            </Button>
+          </div>
+        </Panel>
+      </ReactFlow>
+    </div>
+  );
+}
