b125822d8706b6fc3486fd7a2e92e89cff5ed112
Richie911 <oohrichmond.k@gmail.com>
2025-06-11 02:53:43 +0000

    Update types

--- types/types.ts
+++ types/types.ts
@@ -20,12 +20,19 @@ export type FieldType =
 export type RouteMethods = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'GET_ID';
 
 // Remove the Route type and update EditableData
-export type EditableData = {
+export type WorkflowProps = {
     name: string;
     routes: RouteMethods[]; // Changed from Route[] to RouteMethods[]
     props: Properties[];
     relations: Relation[];
 };
+
+export type WorkflowsProps = {
+    id: string;
+    name: string;
+    workflows: WorkflowProps[]
+    cors?: CorsOptionsCustom;
+}
 
 export type ValidationRule =
     | { type: 'minLength'; value: number }
@@ -87,3 +94,50 @@ export const ValidationTypes = [
     'endsWith',
     'custom',
 ];
+
+type StaticOrigin = boolean | string | RegExp | (string | RegExp)[];
+
+type CorsHttpMethod =
+    | 'GET'
+    | 'POST'
+    | 'PUT'
+    | 'DELETE'
+    | 'PATCH'
+    | 'OPTIONS'
+    | 'HEAD'
+    | 'CONNECT'
+    | 'TRACE';
+
+type CorsAllowedHeader =
+    | 'Accept'
+    | 'Authorization'
+    | 'Content-Type'
+    | 'Origin'
+    | 'X-Requested-With'
+    | 'Access-Control-Allow-Origin'
+    | 'Access-Control-Allow-Headers'
+    | 'Cache-Control'
+    | 'Pragma'
+
+type CorsExposedHeader =
+    | 'Content-Length'
+    | 'X-Knowledge-Base-Version'
+    | 'X-Request-ID'
+    | 'X-RateLimit-Limit'
+    | 'X-RateLimit-Remaining'
+    | 'X-RateLimit-Reset'
+    | 'Authorization'
+    | string; // fallback for custom headers
+
+export type CorsOptionsCustom = {
+    origin?: StaticOrigin;
+    methods?: CorsHttpMethod | CorsHttpMethod[];
+    allowedHeaders?: CorsAllowedHeader | CorsAllowedHeader[];
+    exposedHeaders?: CorsExposedHeader | CorsExposedHeader[];
+    credentials?: boolean;
+    maxAge?: number;
+    preflightContinue?: boolean;
+    optionsSuccessStatus?: number;
+};
+
+export type Relations = "one-to-one" | "one-to-many" | "many-to-many";
