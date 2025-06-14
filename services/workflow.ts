9abe56aa6123878114fb125fccc21da814a068f5
Richie911 <oohrichmond.k@gmail.com>
2025-06-14 23:02:57 +0000

    add workflow api service

---
+++ services/workflow.ts
@@ -0,0 +1,10 @@
+import { WorkflowsProps } from '@/types/types';
+import { postData } from './api';
+
+// Call your real backend endpoint, e.g., /workflows
+export const saveWorkflow = async (workflow: WorkflowsProps) => {
+    return await postData<WorkflowsProps, { message: string }>(
+        '/workflows/generate',
+        workflow
+    );
+};
