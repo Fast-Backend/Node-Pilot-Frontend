60a5338355b9dcbb32d48f9d71101396cbe14feb
Richie911 <oohrichmond.k@gmail.com>
2025-06-27 14:53:46 +0000

    update api url

--- services/api.ts
+++ services/api.ts
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: `${process.env.NEXT_PUBLIC_API_BASE_URL}/api` || 'http://localhost:3001/api',
+    baseURL: `${process.env.NEXT_PUBLIC_API_BASE_URL}api` || 'http://localhost:3001/api',
     headers: {
         'Content-Type': 'application/json',
     },
